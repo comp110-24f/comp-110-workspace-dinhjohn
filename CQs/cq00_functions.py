001115f2c670617c19346d2b8d267ab05f7fdd20
dinhjohn <dinhj@unc.edu>
2024-08-29 12:34:23 -0400

    Finished Exercise 00

---
+++ CQs/cq00_functions.py
@@ -0,0 +1,26 @@
+"""CQ00 Functions"""
+
+__author__ = "730546588"
+
+
+# Defining mimic function
+def mimic(message: str) -> str:
+    """Return back a message"""
+    return message
+
+
+# Calling the mimic function
+print(mimic(message="hellooo"))
+
+
+# Defining main function
+def main() -> None:
+    """Return main function"""
+
+
+# Calling main function
+print(mimic(message=input("What is your message")))
+
+
+if __name__ == "__main__":
+    main()
---
+++ lessons/functions.py
@@ -0,0 +1,12 @@
+"""Practice with functions"""
+
+# from slide 13; define a fucniton
+
+
+def sum(num1: int, num2: int) -> int:
+    """Return num1 + num2."""
+    return num1 + num2
+
+
+# Call the function
+print(sum(num1=1, num2=10))  # <- arguments
